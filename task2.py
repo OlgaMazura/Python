3057054c90afa91956540b89b42a2514885adb57
OlgaMazura <ofmazura@gmail.com>
2023-06-16 12:23:54 +0200

    task2

---
+++ task2.py
@@ -0,0 +1,10 @@
+#Задача 2: Найдите сумму цифр трехзначного числа.
+#Пример: 123 -> 6 (1 + 2 + 3), 100 -> 1 (1 + 0 + 0)
+
+a = str(input('Enter three-digit number: '))
+
+x = int(a[0])
+y = int(a[1])
+z = int (a[2])
+sum = x + y + z
+print(f'Summ of the numbers {x} {y} {z} is --> {sum}')
